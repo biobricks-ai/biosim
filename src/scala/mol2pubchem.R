6fa4643daccb10da3efbc41704c3db7151be0994
Thomas Luechtefeld <tom@insilica.co>
2023-01-08 20:41:57 -0500

    updated mol2pubchem

--- src/scala/mol2pubchem.R
+++ src/scala/mol2pubchem.R
@@ -3,19 +3,21 @@ pacman::p_load(rJava,tidyverse)
 .jinit(classpath = "fingerprinters.jar")
 .jaddClassPath("fingerprinters.jar")
 
-mol2pubchem <- function(smiles){
+pubchembuilder <- function(){
   sbuilder <- .jcall("org/openscience/cdk/silent/SilentChemObjectBuilder",
                         "Lorg/openscience/cdk/interfaces/IChemObjectBuilder;",
                         "getInstance")
+  
   parser = .jnew("org/openscience/cdk/smiles/SmilesParser", sbuilder)
+  fp = .jnew("org/openscience/cdk/fingerprint/PubchemFingerprinter", sbuilder)
 
-  l = map(smiles, \(smi){
+  smiles2pubchem <- function(smi){
     mol = parser$parseSmiles(smi)
-    fp = .jnew("org/openscience/cdk/fingerprint/PubchemFingerprinter", sbuilder)
     bfp = fp$getBitFingerprint(mol)
     zeros = rep(0,881)
     zeros[bfp$getSetbits()] = 1
     zeros
-  })
-  do.call(rbind,l)
+  }
+  
+  purrr::possibly(smiles2pubchem, otherwise = NA)
 }
